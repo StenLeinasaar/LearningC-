7745b752edd5b44f6b5c61b91d67668487627c48
Sten Leinasaar <stenleinasaar@gmail.com>
2023-02-25 21:16:21 -0500

    Revert "first commit"
    This reverts commit ccd015908b0dc20701fc3db2a8ce46416fab8956.

--- person.cs
+++
@@ -1,8 +0,0 @@
-namespace LearningCSharp;
-public class Person
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public int Age { get; set; }
-}
