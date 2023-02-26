2f6e93de73fc2a1a56ce76f658aa170f70a88b60
Sten Leinasaar <stenleinasaar@gmail.com>
2023-02-25 21:13:13 -0500

    homework for Ankit

--- main.cs
+++ main.cs
@@ -3,6 +3,7 @@ using LearningCSharp;
 Person me = new Person();
 me.FirstName = "Matt";
 me.LastName = "Milner";
+// Ankit add a line here  me.Status = "lol";
 
 Console.WriteLine("Hello from, {0} {1}, to you!", 
     me.FirstName, me.LastName);
